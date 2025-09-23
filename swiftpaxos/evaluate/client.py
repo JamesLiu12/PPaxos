4ea931ad1974586ad0bf4a788617cf1b09febc34
root <root@RoxwellLaptop.localdomain>
2025-09-23 15:25:26 +0800

    fix: log

--- swiftpaxos/evaluate/client.py
+++ swiftpaxos/evaluate/client.py
@@ -8,7 +8,10 @@ class Client(Node):
         self.protocol = protocol
 
     def run(self):
-        self.run_cmd(f"mkdir -p /mnt/nfs/paxos/{Node.start_time}/{self.protocol}/{self.alias}")
+        log_dir = os.path.join(Node.nfs_client_path, Node.start_time, self.protocol)
+        log_file = os.path.join(log_dir, self.alias)
+        self.run_cmd(f"mkdir -p {log_dir}")
+        self.run_cmd(f"touch {log_file}")
         return self.run_cmd(f"{os.path.join(Node.working_dir, 'swiftpaxos')}", 
                             "-run client", 
                             f"-config {self.config_path}", 
--- swiftpaxos/evaluate/server.py
+++ swiftpaxos/evaluate/server.py
@@ -8,11 +8,16 @@ class Server(Node):
         self.protocol = protocol
 
     def run(self):
+        log_dir = os.path.join(Node.nfs_client_path, Node.start_time, self.protocol)
+        log_file = os.path.join(log_dir, self.alias)
+        self.run_cmd(f"mkdir -p {log_dir}")
+        self.run_cmd(f"touch {log_file}")
         return self.run_cmd(f"{os.path.join(Node.working_dir, 'swiftpaxos')}", 
                             "-run server", 
                             f"-config {self.config_path}", 
                             f"-alias {self.alias}",
-                            f"-protocol {self.protocol}")
+                            f"-protocol {self.protocol}",
+                            f"-log /mnt/nfs/paxos/{Node.start_time}/{self.protocol}/{self.alias}")
     
     def kill(self):
         return self.run_cmd("pkill -f swiftpaxos")
