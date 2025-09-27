db20241ec579dda49c704fb23bcbb9875113d074
Ubuntu <ubuntu@ip-172-31-3-26.ap-east-1.compute.internal>
2025-09-27 06:58:44 +0000

    feat: complete dump latency cdf

--- swiftpaxos/evaluate/aggregate/dump_latency_cdf.py
+++ swiftpaxos/evaluate/aggregate/dump_latency_cdf.py
@@ -3,15 +3,7 @@ from typing import List, Dict, Optional
 from datetime import datetime
 import yaml
 
-proto_latencies: Dict[str, List[float]]
-
-min_latency = 9999999999.0
-min_proto = None
-min_client = None
-
-max_latency = 0.0
-max_proto = None
-max_client = None
+proto_latencies: Dict[str, List[float]] = {}
 
 data_files = [f'out/conflict{i * 10}.yaml' for i in range(1)]
 
@@ -20,23 +12,29 @@ conflict = 0
 for data_file in data_files:
     data = load_from_yaml(data_file)
 
-    throughputs: Dict[str, Dict[str, float]] = {}
-
     for proto, clients in data.items():
         proto_latencies[proto] = []
         for client, entries in clients.items():
             latencies = [entry.rtt for entry in entries]
             proto_latencies[proto] += latencies
-            latency = sum(latencies) / len(latencies)
-            
-            if latency < min_latency:
-                min_latency = latency
-                min_proto = proto
-                min_client = client
-            
-            if latency > max_latency:
-                max_latency = latency
-                max_proto = proto
-                max_client = client
 
     conflict += 10
+
+proto_latency_cdf: Dict[str, List[tuple]] = {}
+
+for proto, latencies in proto_latencies.items():
+    latencies = sorted(latencies)
+    proto_latency_cdf[proto] = [(latencies[0], 0)]
+    num_chunks = 10
+    stride = len(latencies) // num_chunks
+    left  = len(latencies) % num_chunks
+    index = -1
+    while index < len(latencies) - 1:
+        index += stride
+        if left:
+            index += 1
+            left -= 1
+        proto_latency_cdf[proto].append((latencies[index], (index + 1) / len(latencies)))
+
+with open('out/proto_latency_cdf.yaml', 'w', encoding='utf-8') as f:
+    yaml.safe_dump(proto_latency_cdf, f, sort_keys=False)
