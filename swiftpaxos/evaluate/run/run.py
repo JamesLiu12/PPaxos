001851dbb940bc5c7e568f651d62e32e7a0e642e
root <root@RoxwellLaptop.localdomain>
2025-09-29 14:31:49 +0800

    feat: wait until finish

--- swiftpaxos/evaluate/run/run.py
+++ swiftpaxos/evaluate/run/run.py
@@ -2,11 +2,18 @@ from evaluate.node import Node
 import threading
 from typing import List, Dict
 from .config_loader import ConfigLoader
+import time
 
 def run_node(node: Node):
     node.kill()
     node.init_repo()
     node.run()
+
+def check_status(node: Node):
+    while True:
+        time.sleep(60000)
+        if not node.is_running():
+            return
 
 if __name__ == "__main__":
     config_loader = ConfigLoader()
@@ -20,6 +27,28 @@ if __name__ == "__main__":
     # Clients
     for client in config_loader.clients:
         threads.append(threading.Thread(target=run_node, args=(client,)))
+
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
+    threads = []
+
+    for client in config_loader.clients:
+        threads.append(threading.Thread(target=check_status, args=(client,)))
+
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
+    nodes = [config_loader.master] + config_loader.master + config_loader.clients
+
+    threads = []
+
+    for client in config_loader.clients:
+        threads.append(threading.Thread(target=lambda node: node.kill(), args=(client,)))
 
     for t in threads:
         t.start()
