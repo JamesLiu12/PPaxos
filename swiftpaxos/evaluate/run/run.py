3d9d38973daf9c71544c6077107716f219418048
root <root@RoxwellLaptop.localdomain>
2025-10-05 11:40:10 +0800

    more threads

--- swiftpaxos/evaluate/run/run.py
+++ swiftpaxos/evaluate/run/run.py
@@ -5,10 +5,12 @@ from .config_loader import ConfigLoader
 import time
 
 def run_node(node: Node):
-    node.kill()
-    node.init_repo()
     node.run()
     print('Node ran')
+
+def init_node(node: Node):
+    node.kill()
+    node.init_repo()
 
 def check_status(node: Node):
     while True:
@@ -20,6 +22,25 @@ if __name__ == "__main__":
     config_loader = ConfigLoader()
 
     threads: List[threading.Thread] = []
+
+    # Master
+    threads.append(threading.Thread(target=init_node, args=(config_loader.master,)))
+    # Servers
+    for server in config_loader.servers:
+        threads.append(threading.Thread(target=init_node, args=(server,)))
+    # Clients
+    for client in config_loader.clients:
+        threads.append(threading.Thread(target=init_node, args=(client,)))
+
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
+    print("All initiated")
+
+    threads = []
+
     # Master
     threads.append(threading.Thread(target=run_node, args=(config_loader.master,)))
     # Servers
