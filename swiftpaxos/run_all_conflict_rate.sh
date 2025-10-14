efd1eff7b6c73a4e84aabaf8f3ca3312274b2414
Ubuntu <ubuntu@ip-172-31-33-222.ap-east-1.compute.internal>
2025-10-14 00:50:12 +0000

    eppaxos

--- swiftpaxos/run_all_conflict_rate.sh
+++ swiftpaxos/run_all_conflict_rate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for c in {4..4}; do
+for c in {0..10}; do
     conflict=$((c * 10))
     python3 -m evaluate.run.change_conflict "$conflict"
-    python3 -m evaluate.run.change_proto "paxos"
-    python3 -m evaluate.run.change_all "$conflict" "paxos"
+    python3 -m evaluate.run.change_proto "eppaxos"
+    python3 -m evaluate.run.change_all "$conflict" "eppaxos"
     python3 -m evaluate.run.run
     python3 -m evaluate.run.kill_all
 
